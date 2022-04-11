1d45b3476308becae932e87195ea866759c776c4
Santiago Neira <santiagoneira52@gmail.com>
2022-04-10 23:46:43 -0300

    adding and fix betty of functions handle strings

--- string_tools.c
+++ string_tools.c
@@ -1,71 +1,42 @@
 #include "main.h"
 
 /**
- *-putchar - writes the character c to stdout
- * @c: The character to print.
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
-* *_strcat - function tha concatenate two strings
+**_strcat - function tha concatenate two strings
 * @dest : place where append de src string
 * @src: place of the string for append
 * Return: pointer to dest
 */
 
-<<<<<<< HEAD
-char* _strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-        int dest_length = 0, src_length = 0, i = 0;
-        char *new_string = NULL;
+	int dest_length = 0, src_length = 0, i = 0;
+	char *new_string = NULL;
 
-        while (dest[dest_length] != '\0')
-                dest_length++;
+	while (dest[dest_length] != '\0')
+		dest_length++;
 
-        while (src[src_length] != '\0')
-                src_length++;
+	while (src[src_length] != '\0')
+		src_length++;
 
-        new_string = calloc(2, (dest_length + src_length + 1) * sizeof(char));
+	new_string = calloc(2, (dest_length + src_length + 1) * sizeof(char));
 
-        while (dest_length > i)
-        {
-                new_string[i] = dest[i];
-                i++;
-        }
+	while (dest_length > i)
+	{
+		new_string[i] = dest[i];
+		i++;
+	}
 
-        i = 0;
+	i = 0;
 
-        while (src_length > i)
-        {
-                new_string[dest_length + i] = src[i];
-                i++;
-        }
-=======
-*_strcat(char *dest, char *src)
-{
-	char *start = NULL;
+	while (src_length > i)
+	{
+		new_string[dest_length + i] = src[i];
+		i++;
+	}
 
-	if ((dest == NULL) && (src == NULL))
-		return (NULL);
-	start = dest;
-	while (*start != '\0')
-		start++;
-	while (*src != '\0')
-		*start++ = *src++;
-	*start = '\0';
-	return (dest);
-}
->>>>>>> 3e736fd96780d6df7d50193902fc7f5ba4ef7d9f
+	new_string[dest_length + i + 1] = '\0';
 
-        new_string[dest_length + i + 1] = '\0';
-
-        return (new_string);
+	return (new_string);
 }
 
 /**
@@ -83,8 +54,8 @@ int _strlen(char *s)
 	return (c);
 }
 
-/*
-_strcmp - compare two strings
+/**
+*_strcmp- compare two strings
 *@s1: string one to compare
 *@s2: string two to compare
 * Return: 0 if strings are equal
@@ -101,12 +72,12 @@ int _strcmp(char *s1, char *s2)
 	return (*s1 - *s2);
 }
 
-/*
-_strcmp - compare two strings n words
+/**
+*_strncmp- compare two strings n words
 * @s1: string one
 * @s2: string two
 * @n: number the strings to compare
-* Return: 0 if strings are equal <0 s1 less than str2 >0 s2 than s1 
+*Return: 0 if strings are equal <0 s1 less than str2 >0 s2 than s1
 */
 
 int _strncmp(const char s1[], const char s2[], size_t n)
@@ -116,12 +87,14 @@ int _strncmp(const char s1[], const char s2[], size_t n)
 	unsigned char c2 = '\0';
 	int i;
 
-	for (i = 0; i < (int) n; i++) {
+	for (i = 0; i < (int) n; i++)
+	{
 
 		c1 = (unsigned char) s1[i];
 		c2 = (unsigned char) s2[i];
 
-		if ((c1 == '\0') || (c1 != c2)) {
+		if ((c1 == '\0') || (c1 != c2))
+		{
 
 			return (c1 - c2);
 
@@ -131,7 +104,7 @@ int _strncmp(const char s1[], const char s2[], size_t n)
 	return (c1 - c2);
 }
 
-/** 
+/**
  * *_strcpy - return the copy pointed string por src
  * @dest: copy source to here
  * @src: source for copy
@@ -151,23 +124,5 @@ char *_strcpy(char *dest, char *src)
 
 	return (dest);
 
-}
-
-/**
-* _strcmp - function that compares two strings
-* @s1 : number one to compare
-* @s2: number two to compare
-* Return: 0 is numbers are equal result of sustraction if not.
-*/
-
-int _strcmp(char *s1, char *s2)
-{
-	while (*s1 == *s2 && (*s1 != '\0' && *s2 != '\0'))
-	{
-		s1++;
-		s2++;
-	}
-
-	return (*s1 - *s2);
 }
 
